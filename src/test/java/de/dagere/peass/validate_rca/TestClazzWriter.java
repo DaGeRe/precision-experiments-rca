01af92d2d6e54c8a9c7bf9d57e985d890a648e11
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-09-22 12:43:02 +0200

    Add test for workload level

--- src/test/java/de/dagere/peass/validate_rca/TestClazzWriter.java
+++ src/test/java/de/dagere/peass/validate_rca/TestClazzWriter.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,5 +38,20 @@ public class TestClazzWriter {
       List<ClassOrInterfaceDeclaration> clazzes = unit.findAll(ClassOrInterfaceDeclaration.class);
       ClassOrInterfaceDeclaration exampleClazz = clazzes.get(0);
       Assert.assertEquals(EXAMPLE_CLASS_NAME, exampleClazz.getNameAsString());
+   }
+   
+   @Test
+   public void testWorkloadCallClazzWriter() throws IOException {
+      ClazzWriter writer = new ClazzWriter(new SlowerNodeInfos(2, 1, 0), 1, "ADD");
+      writer.createClass(2, clazzFolder, EXAMPLE_CLASS_NAME, 0, 1, new int[] {5, 6});
+      
+      CompilationUnit unit = JavaParserProvider.parse(new File(clazzFolder, "MyExampleClass.java"));
+      Assert.assertNotNull(unit);
+      
+      List<ClassOrInterfaceDeclaration> clazzes = unit.findAll(ClassOrInterfaceDeclaration.class);
+      ClassOrInterfaceDeclaration exampleClazz = clazzes.get(0);
+      Assert.assertEquals(EXAMPLE_CLASS_NAME, exampleClazz.getNameAsString());
+      
+      MatcherAssert.assertThat(unit.toString(), Matchers.containsString("rm.addSomething(5)"));
    }
 }
