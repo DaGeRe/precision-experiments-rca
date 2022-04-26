d1eea5db167cc232b446fccbb58b09f7c1d2ea92
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-04-26 11:22:22 +0200

    Adapt to changed Peass interface

--- scripts/runProbeOverhead.sh
+++ scripts/runProbeOverhead.sh
@@ -1,7 +1,13 @@
 function createExecutionfile {
     version=$1
     file=$2
-	echo '{"url" : "",  "versions" : {    "'$version'" : {      "testcases" : {        "de.dagere.peass.MainTest" : [ "testMe" ]      },      "predecessor" : "'$version'~1"    }  },  "android" : false}' > $file 
+       echo '{"url" : "", 
+              "versions" : {
+                 "'$version~1'" : {},
+                 "'$version'" : {"testcases" : {"de.dagere.peass.MainTest" : [ "testMe" ] }, "predecessor" : "'$version'~1"}
+              },
+              "android" : false}' > $file 
+
 }
 
 function getFolder {
