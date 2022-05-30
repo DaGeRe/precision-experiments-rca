e05725fffc466d5668d805ecba93c373322ccf7d
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-05-30 18:22:21 +0200

    Analyze all worloads

--- scripts/analysis_strategy/localStrategyComparison.sh
+++ scripts/analysis_strategy/localStrategyComparison.sh
@@ -47,34 +47,30 @@ parallelFolder=$1
 mkdir results
 for depth in 2 4 6 8
 do
-        for percent in 1.003 1.010 1.020 1.030 1.050
+        for percent in 1.010 1.020 1.030 1.050
         do
-                #inputFolder=$parallelFolder/LEVELWISE/$depth/$depth"_LEVELWISE_"$percent"_ADD_1"/project_peass
-                #eventuallyAnalyze $inputFolder results/LEVELWISE_1/$depth/$percent
-                #inputFolder=$parallelFolder/LEVELWISE/$depth/$depth"_LEVELWISE_"$percent"_ADD_2"/project_peass
-                #eventuallyAnalyze $inputFolder results/LEVELWISE_2/$depth/$percent
-                #inputFolder=$parallelFolder/COMPLETE/$depth/$depth"_COMPLETE_"$percent"_ADD_1"/project_peass
-                #eventuallyAnalyze $inputFolder results/COMPLETE/$depth/$percent
-                #inputFolder=$parallelFolder/UNTIL_SOURCE_CHANGE/$depth/$depth"_UNTIL_SOURCE_CHANGE_"$percent"_ADD_1"/project_peass
-                #eventuallyAnalyze $inputFolder results/UNTIL_SOURCE_CHANGE/$depth/$percent
-
-		for strategy in LEVELWISE COMPLETE UNTIL_SOURCE_CHANGE
+		for workload in ADD RAM SYSOUT
 		do
-			inputFolder=$parallelFolder/$strategy/$depth/$depth"_"$strategy"_"$percent"_ADD_1"/project_peass
-                	eventuallyAnalyze $inputFolder results/$strategy/$depth/$percent
-                
-			echo "Test $strategy"
-			resultfile=results/$strategy/$depth/$percent"_outlierRemoval"/depeassMainTest_testMe.pdf
-			if [ -f $resultfile ]
-			then
-				cp $resultfile results/$strategy"_"$depth"_"$percent".pdf"
-				echo "$resultfile copied to results/$strategy"_$depth"_"$percent".pdf"
-			else
-				echo "$resultfile not found and could not be copied"
-			fi
+			for strategy in LEVELWISE COMPLETE UNTIL_SOURCE_CHANGE
+			do
+				inputFolder=$parallelFolder/$strategy/$depth/$depth"_"$strategy"_"$percent"_"$workload"_1"/project_peass
+		        	outputFolder=results/$strategy/$workload/$depth
+		        	eventuallyAnalyze $inputFolder $outputFolder/$percent
+		        
+				echo "Test $strategy"
+				resultfile=$outputFolder/$percent"_outlierRemoval"/depeassMainTest_testMe.pdf
+				if [ -f $resultfile ]
+				then
+					goalFile=results/$strategy"_"$depth"_"$workload"_"$percent".pdf"
+					cp $resultfile $goalFile
+					echo "$resultfile copied to $goalFile"
+				else
+					echo "$resultfile not found and could not be copied"
+				fi
+			done
+			wait
 		done
         done
-        wait
 done
 
 
