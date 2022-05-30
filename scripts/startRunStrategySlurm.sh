cfbe3f2115b161bb925036a33be8871c7dbd6621
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-05-30 23:30:38 +0200

    Adapt to changed parameters

--- scripts/startRunStrategySlurm.sh
+++ scripts/startRunStrategySlurm.sh
@@ -1,16 +1,16 @@
-mkdir -p /nfs/user/do820mize/processlogs/precision-rca/
+mkdir -p /home/sc.uni-leipzig.de/do820mize/processlogs/precision-rca
 for workload in ADD RAM SYSOUT
 do
 	for strategy in UNTIL_SOURCE_CHANGE LEVELWISE COMPLETE
 	do
 		for nodes in 2 4 6 8
 		do
-			for diff in 0.3 1.0 2.0 3.0 5.0
+			for diff in 1.0 2.0 3.0 5.0
 			do
 			slower=$((nodes-1))
 			repetitions=1000000
-			iterations=10
-			vms=1000
+			iterations=100
+			vms=100
 			levels=1
 			export RCA_STRATEGY=$strategy
 			logfolder=/home/sc.uni-leipzig.de/do820mize/processlogs/precision-rca/$workload/
