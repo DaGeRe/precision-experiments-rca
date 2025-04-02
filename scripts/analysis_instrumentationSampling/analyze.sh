974fa8edb9d83f16ac8afa81eec0a6512f956959
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-04-02 12:08:38 +0200

    Add method for getting single values, which is the base for histogram plotting

--- scripts/analysis_instrumentationSampling/analyze.sh
+++ scripts/analysis_instrumentationSampling/analyze.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+function getSum {
+  awk -vOFMT=%.10g '{sum += $1; square += $1^2} END {print sqrt(square / NR - (sum/NR)^2)" "sum/NR" "NR}'
+}
+
 function getHeatmapData {
 	index=$1
 	outname=$2
@@ -31,6 +35,26 @@ do
 	cat $folder/rca/treeMeasurementResults/*/MainTest/testMe.json \
 		 | jq -r '.nodes.statistic | "\((.deviationOld / .meanOld * 1000000 | floor) / 1000000) \((.deviationCurrent / .meanCurrent * 1000000 | floor) / 1000000) \((.tvalue * 1000000 | floor) / 1000000)"'
 done | sort -n -k 1,2 > relativeDeviations.csv 
+
+mkdir -p valueAnalysis
+
+for folder in *_peass
+do
+	shortname=$(echo $folder | awk -F'[_]' '{print $2"_"($3/300-1)*100}' | tr -d "\n")
+	cat $folder/rca/treeMeasurementResults/*/MainTest/details/testMe.json \
+		| jq ".nodes.values.values" \
+		| jq 'to_entries | map({key: .key, mean: (.value | map(.mean) | add / length)})' \
+		| jq ".[] | .mean / 1000000" &> valueAnalysis/values_$shortname.csv
+		
+	cat $folder/rca/treeMeasurementResults/*/MainTest/details/testMe.json \
+		| jq ".nodes.valuesPredecessor.values" \
+		| jq 'to_entries | map({key: .key, mean: (.value | map(.mean) | add / length)})' \
+		| jq ".[] | .mean / 1000000" &> valueAnalysis/predecessor_$shortname.csv
+
+#	echo $folder | awk -F'[_]' '{print $2, ($3/300-1)*100" "}' | tr -d "\n"
+#	cat valueAnalysis/values_$shortname.csv | getSum | tr "\n" " "
+#	cat valueAnalysis/predecessor_$shortname.csv | getSum
+done
 
 for folder in *_peass
 do
